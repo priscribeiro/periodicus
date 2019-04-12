88ff8ac960cf2653c0dacdccc17def5545acbbed
Priscilla <prsc.ribeiro@gmail.com>
2019-04-11 21:08:08 -0300

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <img src="" alt="hidrogenio" />
+      <div>
+        <h2>Hidrogênio</h2>
+        <h3>H</h3>
+        <p>1,00u</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
